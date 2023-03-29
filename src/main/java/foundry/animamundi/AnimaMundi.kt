98c55f3ff2614e6bd6138ed33c04d8006863475e
amo <lunarteclipse@gmail.com>
2023-03-29 19:26:48 +0100

    it runs now

--- src/main/java/foundry/animamundi/AnimaMundi.kt
+++ src/main/java/foundry/animamundi/AnimaMundi.kt
@@ -8,7 +8,6 @@ import foundry.animamundi.content.compat.alembic.CodecUtil
 import foundry.animamundi.grimoire.AnimaMundiEntities
 import foundry.animamundi.grimoire.AnimaMundiItems.register
 import foundry.animamundi.grimoire.AnimaMundiNetworking
-import foundry.animamundi.happenings.ForgeCommonEvents
 import net.minecraft.resources.ResourceLocation
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.TickEvent.ServerTickEvent
@@ -19,36 +18,46 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
-import thedarkcolour.kotlinforforge.forge.MOD_BUS
-import thedarkcolour.kotlinforforge.forge.runForDist
 
 @Mod(AnimaMundi.MODID)
-object AnimaMundi {
-    public const val MODID = "animamundi"
-    public val LOGGER = LogUtils.getLogger()
-    public val REGISTRATE = NonNullSupplier.lazy { Registrate.create(MODID) }
-
-    fun registrate(): Registrate {
-        return REGISTRATE.get()
-    }
-
-    fun path(path: String?): ResourceLocation {
-        return ResourceLocation(MODID, path)
-    }
-
+class AnimaMundi {
     init {
+        MinecraftForge.EVENT_BUS.register(this)
+        val modEventBus = FMLJavaModLoadingContext.get().modEventBus
         CodecUtil.init()
         register()
         AnimaMundiEntities.register()
         AnimaMundiNetworking.init()
-
-        val obj = runForDist(
-            clientTarget = {},
-            serverTarget = {
-                MOD_BUS.addListener(ForgeCommonEvents::attachCaps)
-                MOD_BUS.addListener(ForgeCommonEvents::playerTick)
-            }
-        )
     }
 
+    private fun commonSetup(event: FMLCommonSetupEvent) {}
+    @SubscribeEvent
+    fun onServerStarting(event: ServerStartingEvent?) {
+    }
+
+    @SubscribeEvent
+    fun onServerTick(event: ServerTickEvent?) {
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
+    object ClientModEvents {
+        @SubscribeEvent
+        fun onClientSetup(event: FMLClientSetupEvent?) {
+        }
+    }
+
+    companion object {
+        const val MODID = "animamundi"
+        private val LOGGER = LogUtils.getLogger()
+        val REGISTRATE = NonNullSupplier.lazy { Registrate.create(MODID) }
+        @JvmStatic
+        fun registrate(): Registrate {
+            return REGISTRATE.get()
+        }
+
+        @JvmStatic
+        fun path(path: String?): ResourceLocation {
+            return ResourceLocation(MODID, path)
+        }
+    }
 }
